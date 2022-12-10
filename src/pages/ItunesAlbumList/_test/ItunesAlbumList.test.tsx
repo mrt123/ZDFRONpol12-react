1296a9cd17adc9b56009135ac1130faa335c26fe
Tomasz Sz <tomasz.szawara@gmail.com>
2022-12-10 12:28:16 +0100

    WIP2: making of the ItunesAlbumList.test

--- src/pages/ItunesAlbumList/_test/ItunesAlbumList.test.tsx
+++ src/pages/ItunesAlbumList/_test/ItunesAlbumList.test.tsx
@@ -1,5 +1,6 @@
 import { render, waitFor } from "@testing-library/react";
 import ItunesAlbumList from "../ItunesAlbumList";
+import { ItunesAlbumDataEntry } from "../itunesDataTransformer";
 
 describe("ItunesAlbumList", () => {
   it("matches snapshot when data is loading", () => {
@@ -8,20 +9,66 @@ describe("ItunesAlbumList", () => {
   });
 
   it("matches snapshot after data is loaded", async () => {
+    // ARRANGE
+    const rawAlbumEntires: ItunesAlbumDataEntry[] = [
+      {
+        "im:name": {
+          label: "Album1",
+        },
+        "im:artist": {
+          label: "Michael Jackson",
+        },
+        "im:releaseDate": {
+          label: "1999/05/01",
+        },
+        "im:price": {
+          attributes: {
+            amount: "99000000",
+          },
+          label: "99000000",
+        },
+      },
+      {
+        "im:name": {
+          label: "Killing me softly",
+        },
+        "im:artist": {
+          label: "Fugees",
+        },
+        "im:releaseDate": {
+          label: "1996/05/27",
+        },
+      },
+    ];
+
+    const rawAlbumTestData = {
+      feed: {
+        entry: rawAlbumEntires,
+      },
+    };
+
+    window.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(rawAlbumTestData),
+      })
+    ) as jest.Mock;
+
+    // ACT
     const { container } = render(<ItunesAlbumList />);
 
     const loaderElements = container.getElementsByClassName("loader");
 
-    console.log("pobralismy loaderElements z containera");
+    await waitFor(
+      () => {
+        return expect(loaderElements.length === 0).toEqual(true);
+      },
+      {
+        timeout: 13000,
+      }
+    );
 
-    await waitFor(() => {
-      console.log(
-        "sprawdzamy ile jest loaderow. Obecnie jest ...",
-        loaderElements.length
-      );
-      return expect(loaderElements.length === 0);
-    });
-
+    // ASSERT
+    // We do not test snapshot here, we avoid complex snapshot comparison. Imagine this is the most complex component in a large application.
     expect(container).toMatchSnapshot();
   });
 });
